057879a6ce415fff0fd972baefc608329708fd89
Hoang Phuc Nguyen <68393604+PhucNguyen-rsc@users.noreply.github.com>
2023-05-09 19:59:31 +0400

    Fixing UI elements

--- Assets/Scripts/SettingBoard.cs
+++ Assets/Scripts/SettingBoard.cs
@@ -6,8 +6,30 @@ using UnityEngine.SceneManagement;
 public class SettingBoard : MonoBehaviour
 {
     [SerializeField] GameObject settingBoard;
+    private GameObject startButton;
+    private GameObject instructionButton;
+    private GameObject settingButton;
+    
+    private void Awake()
+    {
+        startButton = GameObject.Find("Start");
+        settingButton = GameObject.Find("Setting");
+        instructionButton = GameObject.Find("Instruction");
+    }
 
-    public void Music(){
-        SceneManager.LoadScene("GameScene");
+    public void Setting()
+    {
+        settingBoard.SetActive(true);
+        startButton.SetActive(false);
+        settingButton.SetActive(false);
+        instructionButton.SetActive(false);
+    }
+
+    public void BackButton()
+    {
+        settingBoard.SetActive(false);
+        startButton.SetActive(true);
+        settingButton.SetActive(true);
+        instructionButton.SetActive(true);
     }
 }
